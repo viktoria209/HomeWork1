66637379a741b82284dff74ba6f272e966358247
Виктория <209.viktoria@gmail.com>
2023-03-07 10:27:43 +1000

    Programm

--- 12/Program.cs
+++ 12/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static string[] FilterArrayStrings(string[] inputArray)
+{
+  int limitLength = 3;
+  int count = 0;  
+  string[] newArray = new string[inputArray.Length];
+
+  for (int i = 0; i < inputArray.Length; i++)
+  {
+    if (inputArray[i].Length <= limitLength)
+    {
+      newArray[count] = inputArray[i];
+      count++;
+    }
+  }
+
+  return newArray;
+}
+
+void ShowArray(string[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write(array[i] + " ");
+  }
+  Console.WriteLine();
+}
+
+Console.Write("Input number of elements in the array: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string[] inputArray = new string[num];
+for (int i = 0; i < inputArray.Length; i++)
+{
+  Console.Write($"Input {i + 1} string: ");
+  inputArray[i] = Console.ReadLine()!;
+}
+
+string[] newArray = FilterArrayStrings(inputArray);
+
+Console.WriteLine("Strings with length less than or equal to 3:");
+ShowArray(newArray);
